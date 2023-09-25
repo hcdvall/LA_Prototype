26490cbc5a23946d83134183cb42bc2c2e1f0c4c
hcdvall <henrik.sedvall@gmail.com>
2023-09-25 16:37:12 +0200

    Setup player character

---
+++ Source/LA_Prototype/Private/LAPlayerCharacter.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LAPlayerCharacter.h"
+
+#include "EnhancedInputSubsystems.h"
+
+
+ALAPlayerCharacter::ALAPlayerCharacter()
+{
+	
+}
+
+
+
---
+++ Source/LA_Prototype/Public/LAPlayerCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LA_PrototypeCharacter.h"
+#include "LAPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS( Blueprintable, BlueprintType )
+class LA_PROTOTYPE_API ALAPlayerCharacter : public ALA_PrototypeCharacter
+{
+	GENERATED_BODY()
+
+	ALAPlayerCharacter();
+	
+};
